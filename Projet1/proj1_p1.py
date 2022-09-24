f6a4052f80a3bc2dd326374dc2fc7a34ab101785
Yster <wguillaume28@gmail.com>
2022-09-24 12:20:33 +0200

    Quelques modifications esthétiques

--- Projet1/proj1_p1.py
+++ Projet1/proj1_p1.py
@@ -1,11 +1,15 @@
 import numpy as np
 
+# creer dictionnaire (?)
 class Grid ():
 	def __init__(self, att):
 		self.att = att
 
-	def peut_placer(self, grid, bat, pos, dir):
-		# creer dictionnaire (?)
+	def peut_placer(self, grid: Grid, bat: dict(str, int), pos: tuple(int,int), dir: int):
+		""" Grille * dict(str, int) * tuple(int, int) * int -> Bool
+
+			Renvoie sous forme de booléen si le bateau b peut etre placé a la postion et avec la direction indiquées
+		"""	
 
 		lig_pos = pos[0]
 		col_pos = pos[1]
@@ -13,16 +17,17 @@ class Grid ():
 		if dir: 
 			for lig_v in range(lig_pos, lig_pos+bat):
 				if grid[lig_v][col_pos] != 0:
-					print("Bateau impossible a placer : un bateau de type {} est en {}".format(bat, ))
+					print("Bateau impossible a placer : un bateau de type {} est en '{},{}'".format(bat, lig_v, col_pos))
 					return False 
+
 		# dir = 0 --> horizontal
 		else:
 			for col_v in range(col_pos, col_pos+bat):	
 				if grid[lig_pos][col_v] != 0:
-					print("Bateau impossible a placer : un bateau de type {} est en {}".format(bat, ))
+					print("Bateau impossible a placer : un bateau de type {} est en '{},{}'".format(bat, lig_pos, col_v))
 					return False 
 
-	def place(self, grid, bat, pos, dir):
+	def place(self, grid: Grid, bat, pos: tuple(int, int), dir: int):
 		if not self.peut_placer(grid, bat, pos, dir):
 			return grid
 		else:
