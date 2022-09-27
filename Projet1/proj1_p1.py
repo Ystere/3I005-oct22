f5879b935f512e18d1d077c0b27f0a1a82cbd82e
Yster <wguillaume28@gmail.com>
2022-09-27 15:48:29 +0200

    Grid : ajouts dans la classe `peut_placer` : redéfinition de la fonction

--- Projet1/proj1_p1.py
+++ Projet1/proj1_p1.py
@@ -4,30 +4,37 @@ import numpy as np
 class Grid ():
 	def __init__(self, att):
 		self.att = att
+		# Grille
+		mat = np.zeros((10,10), dtype=int)
+		size = len(mat)
 
-	def peut_placer(self, grid: Grid, bat: dict(str, int), pos: tuple(int,int), dir: int):
+	def peut_placer(self, grid: Grid, bat: int, pos: tuple(int,int), dir: int):
 		""" Grille * dict(str, int) * tuple(int, int) * int -> Bool
 
 			Renvoie sous forme de booléen si le bateau b peut etre placé a la postion et avec la direction indiquées
 		"""	
-		# Vérification du possible positionnement du bateau dans la grille
-		size_bat = TODO
-
 		# Placement du bateau
 		lig_pos = pos[0]
 		col_pos = pos[1]
+
+		# Vérification du possible positionnement du bateau dans la grille
+		size_bat = dict_bat_size.get(bat)
+		if lig_pos + size_bat >= grid.size or col_pos + size_bat >= grid.size:
+			print("Le bateau dépasse de la grille et n'est pas positionnable.")
+			return False
+
 		# dir = 1 --> vertical
 		if dir: 
 			for lig_v in range(lig_pos, lig_pos+bat):
 				if grid[lig_v][col_pos] != 0:
-					print("Bateau impossible a placer : un bateau de type {} est en '{},{}'".format(bat, lig_v, col_pos))
+					print("Bateau impossible a placer : un bateau de type {} est en '{},{}'".format(dict_id_bat.get(bat), lig_v, col_pos))
 					return False 
 
 		# dir = 0 --> horizontal
 		else:
 			for col_v in range(col_pos, col_pos+bat):	
 				if grid[lig_pos][col_v] != 0:
-					print("Bateau impossible a placer : un bateau de type {} est en '{},{}'".format(bat, lig_pos, col_v))
+					print("Bateau impossible a placer : un bateau de type {} est en '{},{}'".format(dict_id_bat(bat), lig_pos, col_v))
 					return False 
 
 	def place(self, grid: Grid, bat, pos: tuple(int, int), dir: int):
@@ -52,14 +59,12 @@ class Grid ():
 	
 
 def create_grid():
-	# Grille
-	mat = np.zeros(10, 10, np.int32)
 
 	# Dictionnaire des correspondances bateau-taille
 	dict_bat_size = { 1:5, 2:4, 3:3, 4:3, 5:2 }
 
 	# Dictionnaire des correspondances identifiant-bateau
-	dict_id_bat = { "Porte-Avions":1, "Croiseur":2, "Contre-Torpilleur":3, "Sous-Marin":4, "Torpilleur":5}
+	dict_id_bat = { 1:"Porte-Avions", 2:"Croiseur", 3:"Contre-Torpilleur", 4:"Sous-Marin", 5:"Torpilleur"}
 
 	# Création d'une partie (?)
 	return
