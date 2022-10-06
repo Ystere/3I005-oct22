8e27d900e8a01a90d78f2014b6c2c610cc278d9c
Yster <wguillaume28@gmail.com>
2022-10-06 14:26:13 +0200

    Nouveau Fichier pour la partie 3 et 4

---
+++ Projet1/proj1_3.py
@@ -0,0 +1,29 @@
+import numpy as np
+import matplotlib.pyplot as plt
+import copy
+
+class Battle():
+	def __init__(self):
+		return
+
+	def play(self, position):
+		return
+
+	def victory(self):
+		return
+	
+	def reset(self):
+		return
+	
+
+class RandomPlayer():
+	pass
+
+class HeuristicPlayer():
+	pass
+
+class SimpleP_Player():
+	pass
+
+class MCMethodPlayer():
+	pass
