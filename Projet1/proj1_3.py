6746f38908e562866530e7b1ab2d056ed7c362cf
saadmsd <111648254+saadmsd@users.noreply.github.com>
2022-10-13 14:35:07 +0200

    Modification fonction version aléatoire

--- Projet1/proj1_3.py
+++ Projet1/proj1_3.py
@@ -1,14 +1,15 @@
 import numpy as np
 import matplotlib.pyplot as plt
 import copy
-import proj1_p2
+import math
+import proj1_p2 as g
 
 class Battle():
 	def __init__(self):
 		self.hit_count 		= 0
 		self.attempt_count 	= 0
-		self.rand_grid 		= generate_grid()
-		self.play_grid 		= np.zeros((10, 10), dtype=bool)
+		self.rand_grid 		= g.generate_grid()
+		self.play_grid		= g.Grid()
 
 
 	def play(self, position):
@@ -21,10 +22,12 @@ class Battle():
 
 			Retourne si le coup joué est touché ou non.
 		"""
-		self.attempt_count += 1
-		if not self.play_grid[position[0]][position[1]]:
+		
+		if not self.play_grid.grid[position[0]][position[1]]:
 			print("Coup non joué ici auparavant.")
-			if self.rand_grid[position[0]][position[1]] != 0:
+			self.attempt_count += 1
+			self.play_grid.grid[position[0]][position[1]] = True
+			if self.rand_grid.grid[position[0]][position[1]] != 0:
 				print("Bateau touché !")
 				self.hit_count += 1
 				return True
@@ -36,10 +39,10 @@ class Battle():
 			return False
 
 	def victory(self):
-		return hit_count == 17
+		return self.hit_count == 17
 	def reset(self):
 		self.hit_count = 0
-		self.rand_grid = generate_grid()
+		self.rand_grid = g.generate_grid()
 		print("Partie réinitalisée")
 	
 
@@ -48,8 +51,33 @@ class RandomPlayer():
 	# Jeu a un joueur : 17 cases "remplies" donc nombres minimal de coups a jouer = 17
 	# Espérance = sum([17 <= k <= 100], k*P(X = k))*
 	# A vérifier
+	def __init__(self):
+		self.battle = Battle()
 
-	pass
+	def random_play(self):
+		""" --> int
+
+			Renvoie le nombre de coups joués avant d'avoir touchés tous les bateaux 
+   			dans la grille aléatoire en jouant chaque coup de manière aléatoire
+		"""
+		random_grid = self.battle.rand_grid
+		while not self.battle.victory():	# tant qu'on a pas gagné
+			x = int(np.fix(random_grid.size*np.random.random()))
+			y = int(np.fix(random_grid.size*np.random.random()))
+			self.battle.play((x,y))		# on joue un coup dans une position aléatoire
+		return self.battle.attempt_count
+
+
+#-----Test----
+r = RandomPlayer()
+
+n = r.random_play()
+print(n)
+s=0
+for k in range(17, 101):
+    s = s + k*(math.comb(83, k-17)/math.comb(100, k))
+print(s)
+#-------------
 
 class HeuristicPlayer():
 	pass
